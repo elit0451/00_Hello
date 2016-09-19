d9fa77ad15e150aaddd029bd1d74cb006a748c4d
Elitsa Marinovska <Elitsa Marinovska@DESKTOP-03FMA85>
2016-09-19 22:56:57 +0200

    Add project files.

---
+++ 00_Hello/Functions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _00_Hello
+{
+    internal class Functions
+    {
+        internal static string Hello()
+        {
+            return "hello";
+        }
+
+        internal static object Hello(string v)
+        {
+            return v = "hello," + " " + v ;
+        }
+    }
+    
+}
